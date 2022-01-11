323bc880d986feb80141f3f61142d8c750752358
Kristian Mello <kristian.bonfim@hotmail.com>
2022-01-11 11:33:48 -0300

    Project created

---
+++ backend/src/main/java/com/surfersolution/ssmovies/SsmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.surfersolution.ssmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmoviesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/surfersolution/ssmovies/SsmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surfersolution.ssmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
